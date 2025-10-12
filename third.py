19a44bd75e95036fbc01690daf197c6b763cb80a
Iveta Š <svepei00@jcu.cz>
2025-10-12 16:50:53 +0200

    third.py

--- third.py
+++ third.py
@@ -1,1 +1,22 @@
+def je_prvocislo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
+def vrat_prvocisla(maximum):
+    return [i for i in range(2, maximum + 1) if je_prvocislo(i)]
+
+if __name__ == "__main__":
+    print(je_prvocislo(1))
+    print(je_prvocislo(2))
+    print(je_prvocislo(3))
+    print(je_prvocislo(100))
+    print(je_prvocislo(101))
+
+    print(vrat_prvocisla(1))
+    print(vrat_prvocisla(2))
+    print(vrat_prvocisla(3))
+    print(vrat_prvocisla(10))
