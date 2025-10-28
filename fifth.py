9cff39fabed129e67c5ba580a6fbc8a00410509e
Iveta Š <svepei00@jcu.cz>
2025-10-28 13:18:48 +0000

    Hotové řešení fifth.py -detekce typu obrázku

--- fifth.py
+++ fifth.py
@@ -1,51 +1,42 @@
-
 import sys
 
 # definice úvodních binárních sekvencí obrázkových souborů
-jpeg_header = b'\xff\xd8'
+jpeg_header = b'\xff\xd8\xff'
 gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
 png_header = b'\x89PNG\r\n\x1a\n'
-
 
 def read_header(file_name, header_length):
     """
     Tato funkce načte binární soubor z cesty file_name,
     z něj přečte prvních header_length bytů a ty vrátí pomocí return
     """
-    return b'xxx'
-
+    with open(file_name, "rb") as f:
+        return f.read(header_length)
 
 def is_jpeg(file_name):
     """
     Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanou hlavičkou v proměnné jpeg_header
     """
-    # načti hlavičku souboru
     header = read_header(file_name, len(jpeg_header))
-
-    # vyhodnoť zda je soubor jpeg
-
-    return False
-
+    return header.startswith(jpeg_header)
 
 def is_gif(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    Funkce zkusí přečíst ze souboru hlavičku obrázku gif,
     tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
     """
-    # vyhodnoť zda je soubor gif
-    return False
-
+    header = read_header(file_name, max(len(gif_header1), len(gif_header2)))
+    return header.startswith(gif_header1) or header.startswith(gif_header2)
 
 def is_png(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    Funkce zkusí přečíst ze souboru hlavičku obrázku png,
     tu srovná s definovanou hlavičkou v proměnné png_header
     """
-    # vyhodnoť zda je soubor png
-    return False
-
+    header = read_header(file_name, len(png_header))
+    return header.startswith(png_header)
 
 def print_file_type(file_name):
     """
@@ -60,8 +51,14 @@ def print_file_type(file_name):
     else:
         print(f'Soubor {file_name} je neznámého typu')
 
-
 if __name__ == '__main__':
-    # přidej try-catch blok, odchyť obecnou vyjímku Exception a vypiš ji
-    file_name = sys.argv[1]
-    print_file_type(file_name)
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print("Použití: python fifth.py cesta/k/obrazku")
+        sys.exit(1)
+
+    try:
+        print_file_type(file_name)
+    except Exception as e:
+        print(f"Nastala chyba: {e}")
