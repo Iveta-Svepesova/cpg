f4cf8f4d1880634dfde415ace85cb3da9b2a2644
Iveta Š <svepei00@jcu.cz>
2025-12-03 21:57:01 +0000

    uprava funkce url

--- sixth.py
+++ sixth.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import re
 
 
 def download_url_and_get_all_hrefs(url):
@@ -11,13 +12,32 @@ def download_url_and_get_all_hrefs(url):
     """
     hrefs = []
 
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Chyba: server vratil kod {response.status_code}")
+        return hrefs
+
+    html = response.text
+
+    # vyhledá všechny href odkazy
+    matches = re.findall(r'<a\s+href=["\'](.*?)["\']', html)
+
+    # přidá jen absolutní odkazy
+    for m in matches:
+        if m.startswith("http"):
+            hrefs.append(m)
+
     return hrefs
 
 
 if __name__ == "__main__":
     try:
-        url = sys.argv[1]
-        download_url_and_get_all_hrefs(url)
-    # osetrete potencialni chyby pomoci vetve except
+        # URL ze zadání
+        url = "https://www.jcu.cz"
+        odkazy = download_url_and_get_all_hrefs(url)
+        print(odkazy)  # vypíše seznam odkazů, např.:
+        # ["https://www.jcu.cz/cz/prijimaci-zkousky/studijni-programy",
+        #  "https://www.jcu.cz/cz/prijimaci-zkousky/prijimaci-rizeni", ...]
     except Exception as e:
         print(f"Program skoncil chybou: {e}")
